f8dd43a91f11b32a2c10710136ce4a03545b9164
jtrtj <jt.r@live.com>
2018-09-12 09:44:03 -0600

    add OxfordConnection service to presenter

--- spec/services/validation_presenter_spec.rb
+++ spec/services/validation_presenter_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 describe ValidationPresenter do
   context 'instance methods' do
-    it '#get_result - gets result from Oxford dict' do
+    it '#get_response - gets result from Oxford dict' do
       file = File.read('spec/fixtures/mock_successful_validation.json')
-      expect_result = JSON.parse(file, symbolize_names: true)
+      expect_response = JSON.parse(file, symbolize_names: true)
       validator = ValidationPresenter.new('foxes')
-      expect(validator.get_result).to eq()
+      expect(validator.get_response).to eq(expect_response)
     end
   end
 end
