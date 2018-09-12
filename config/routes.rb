14fc80a0728c35e06372e604fff20e6791e47daf
jtrtj <jt.r@live.com>
2018-09-12 10:43:40 -0600

    add route for api game show

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   root "welcome#index"
   get '/validation', to: 'validations#show'
+
+  namespace :api do
+    namespace :v1 do
+      resources :games, only: [:show]
+    end
+  end
 end
